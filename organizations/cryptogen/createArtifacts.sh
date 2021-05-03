9c6c48ca16e44cc9671c17d0e79c487e03416f5a
jitho <jithovjoy@gmail.com>
2021-05-03 20:13:16 +0530

    created configtx.yaml file for channel artifacts, policies

--- organizations/cryptogen/createArtifacts.sh
+++ organizations/cryptogen/createArtifacts.sh
@@ -8,4 +8,20 @@
 
 
 # Generate Crypto artifacts for organizations
-cryptogen generate --config=./crypto-config.yaml --output=./crypto-config
+
+#cryptogen generate --config=./crypto-config.yaml --output=./crypto-config
+
+SYS_CHANNEL="sys-channel"
+
+CHANNEL_NAME="mychannel"
+
+echo $CHANNEL_NAME
+
+
+#Generate System Genisis Block
+
+configtxgen -profile OrdererGenesis -configPath ../../configtx -channelID $SYS_CHANNEL -outputBlock ../../channel/genisis.block
+
+# Generate channel configuration block
+
+configtxgen -profile BasicChannel -configPath ../../configtx -outputCreateChannelTx ../../channel/mychannel.tx -channelID $CHANNEL_NAME
