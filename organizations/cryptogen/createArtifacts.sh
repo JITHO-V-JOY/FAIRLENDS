ddc6392505d1423324b89b73b75f8b73a1ecb1e3
jitho <jithovjoy@gmail.com>
2021-05-04 13:37:36 +0530

    Generated anchor peer update for organizations

--- organizations/cryptogen/createArtifacts.sh
+++ organizations/cryptogen/createArtifacts.sh
@@ -20,8 +20,21 @@ echo $CHANNEL_NAME
 
 #Generate System Genisis Block
 
-configtxgen -profile OrdererGenesis -configPath ../../configtx -channelID $SYS_CHANNEL -outputBlock ../../channel/genisis.block
+#configtxgen -profile OrdererGenesis -configPath ../../configtx -channelID $SYS_CHANNEL -outputBlock ../../channel/genisis.block
 
 # Generate channel configuration block
 
-configtxgen -profile BasicChannel -configPath ../../configtx -outputCreateChannelTx ../../channel/mychannel.tx -channelID $CHANNEL_NAME
+#configtxgen -profile BasicChannel -configPath ../../configtx -outputCreateChannelTx ../../channel/mychannel.tx -channelID $CHANNEL_NAME
+
+
+echo "######## Generating anchor peer update for Org1MSP #############"
+
+configtxgen -profile BasicChannel -configPath ../../configtx -outputAnchorPeersUpdate ../../channel/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+
+echo "######## Generating anchor peer update for Org2MSP #############"
+
+configtxgen -profile BasicChannel -configPath ../../configtx -outputAnchorPeersUpdate ../../channel/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+
+echo "######## Generating anchor peer update for Org3MSP #############"
+
+configtxgen -profile BasicChannel -configPath ../../configtx -outputAnchorPeersUpdate ../../channel/Org3MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
