a3b8ab641a9f0d104450c71f90aa59c74ed18cab
jitho <jithovjoy@gmail.com>
2021-05-24 00:56:21 +0530

    login page of fairlends

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const auth = require('../controllers/auth');
 
+router.get('/login', function(req, res){
+    res.render('users/login')
+})
 router.post('/signin', auth.signin);
 router.post('/signup', auth.signup);
 router.get("/signout", auth.signout);
