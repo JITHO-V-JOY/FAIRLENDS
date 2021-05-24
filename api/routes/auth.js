26b0c5e058148fb8a90728e9f731914f04b589a2
jitho <jithovjoy@gmail.com>
2021-05-24 13:38:56 +0530

    register page

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -7,6 +7,11 @@ router.get('/login', function(req, res){
     res.render('users/login', {err:false, loggedin:false})
 })
 router.post('/signin', auth.signin);
+
+router.get('/register', function(req, res){
+    res.render('users/register', {err:false, loggedin:false})
+})
+
 router.post('/signup', auth.signup);
 router.get("/signout", auth.signout);
 
