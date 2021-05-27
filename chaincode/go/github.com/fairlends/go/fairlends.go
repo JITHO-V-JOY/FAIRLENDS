2640cc2cd168d771199879bbef0bea95577dca87
jitho <jithovjoy@gmail.com>
2021-05-27 16:46:41 +0530

    fairlends sc updated

--- chaincode/go/github.com/fairlends/go/fairlends.go
+++ chaincode/go/github.com/fairlends/go/fairlends.go
@@ -12,12 +12,7 @@ type SmartContract struct {
 	contractapi.Contract
 }
 
-var logger = flogging.MustGetLogger("fabcar_cc")
-
-type Repayment struct {
-	Date    string `json:"date"`
-	Payment bool   `json:"payment"`
-}
+var logger = flogging.MustGetLogger("fairlends_cc")
 
 type User struct {
 	Adhar_ID string `json:"adhar_id"`
@@ -29,22 +24,6 @@ type User struct {
 	State    string `json:"state"`
 	City     string `json:"city"`
 	Pincode  string `json:"pincode"`
-}
-
-type Loan struct {
-	Loan_ID      string      `json:"loan_id"`
-	Issuer       string      `json:"issuer"`
-	Lender       string      `json:"lender"`
-	Amount       int64       `json:"amount"`
-	Interest     int         `json:"interest"`
-	Tenure       int         `json:"tenure"`
-	Approved     bool        `json:"approved"`
-	IssuedDate   string      `json:"issued_date"`
-	ApprovedDate string      `json:"approved_date"`
-	EndDate      string      `json:"end_date"`
-	Emi          []Repayment `json:"emi"`
-	Tax          []Repayment `json:"tax"`
-	Status       string      `json:"status"`
 }
 
 // ******************************************** Create User function *************************************************************
@@ -71,190 +50,28 @@ func (s *SmartContract) CreateUser(ctx contractapi.TransactionContextInterface, 
 	return ctx.GetStub().GetTxID(), ctx.GetStub().PutState(user.Adhar_ID, userAsBytes)
 }
 
-// ******************************************** Issue Loan function *************************************************************
-
-func (s *SmartContract) IssueLoan(ctx contractapi.TransactionContextInterface, loanData string) (string, error) {
-
-	if len(loanData) == 0 {
-		return "", fmt.Errorf("Please pass the correct loan data")
+// ******************************************** Query User function *************************************************************
+func (s *SmartContract) QueryUser(ctx contractapi.TransactionContextInterface, adhar_id string) (*User, error) {
+	if len(adhar_id) == 0 {
+		return nil, fmt.Errorf("Please pass the correct car data")
 	}
 
-	var loan Loan
-	err := json.Unmarshal([]byte(loanData), &loan)
-	if err != nil {
-		return "", fmt.Errorf("Failed while unmarshling loan. %s", err.Error())
-	}
-
-	loanAsBytes, err := json.Marshal(loan)
-	if err != nil {
-		return "", fmt.Errorf("Failed while marshling car. %s", err.Error())
-	}
-
-	ctx.GetStub().SetEvent("CreateAsset", loanAsBytes)
-
-	return ctx.GetStub().GetTxID(), ctx.GetStub().PutState(loan.Loan_ID, loanAsBytes)
-}
-
-// ******************************************** Adding Lender function *************************************************************
-
-func (s *SmartContract) AddLender(ctx contractapi.TransactionContextInterface, adhar_id string, loan_id string) (string, error) {
-	if len(adhar_id) == 0 || len(loan_id) == 0 {
-		return "", fmt.Errorf("Please pass the correct loan data")
-	}
-
-	loanAsBytes, err := ctx.GetStub().GetState(loan_id)
+	userAsBytes, err := ctx.GetStub().GetState(adhar_id)
 
 	if err != nil {
-		return "", fmt.Errorf("Failed to get loan data. %s", err.Error())
+		return nil, fmt.Errorf("failed to read from world state. %s", err.Error())
+
 	}
 
-	if loanAsBytes == nil {
-		return "", fmt.Errorf("%s does not exist", loan_id)
+	if userAsBytes == nil {
+		return nil, fmt.Errorf("%s does not exist", adhar_id)
+
 	}
 
-	loan := new(Loan)
-	_ = json.Unmarshal(loanAsBytes, loan)
+	user := new(User)
+	_ = json.Unmarshal(userAsBytes, user)
 
-	loan.Lender = adhar_id
-
-	loanAsBytes, err = json.Marshal(loan)
-	if err != nil {
-		return "", fmt.Errorf("Failed while marshling loan. %s", err.Error())
-	}
-
-	return ctx.GetStub().GetTxID(), ctx.GetStub().PutState(loan.Loan_ID, loanAsBytes)
-
-}
-
-// ******************************************** Approve Loan function *************************************************************
-
-func (s *SmartContract) ApproveLoan(ctx contractapi.TransactionContextInterface, loan_id string) (string, error) {
-	if len(loan_id) == 0 {
-		return "", fmt.Errorf("Please pass the correct loan data")
-	}
-
-	loanAsBytes, err := ctx.GetStub().GetState(loan_id)
-
-	if err != nil {
-		return "", fmt.Errorf("Failed to get loan data. %s", err.Error())
-	}
-
-	if loanAsBytes == nil {
-		return "", fmt.Errorf("%s does not exist", loan_id)
-	}
-
-	loan := new(Loan)
-	_ = json.Unmarshal(loanAsBytes, loan)
-
-	loan.Approved = true
-	loan.Status = "Active"
-
-	loanAsBytes, err = json.Marshal(loan)
-	if err != nil {
-		return "", fmt.Errorf("Failed while marshling loan. %s", err.Error())
-	}
-
-	return ctx.GetStub().GetTxID(), ctx.GetStub().PutState(loan.Loan_ID, loanAsBytes)
-}
-
-// ******************************************** Redeem function *************************************************************
-
-func (s *SmartContract) Redeem(ctx contractapi.TransactionContextInterface, loan_id string) (string, error) {
-	if len(loan_id) == 0 {
-		return "", fmt.Errorf("Please pass the correct loan data")
-	}
-
-	loanAsBytes, err := ctx.GetStub().GetState(loan_id)
-
-	if err != nil {
-		return "", fmt.Errorf("Failed to get loan data. %s", err.Error())
-	}
-
-	if loanAsBytes == nil {
-		return "", fmt.Errorf("%s does not exist", loan_id)
-	}
-
-	loan := new(Loan)
-	_ = json.Unmarshal(loanAsBytes, loan)
-
-	loan.Status = "Redeem"
-
-	loanAsBytes, err = json.Marshal(loan)
-	if err != nil {
-		return "", fmt.Errorf("Failed while marshling loan. %s", err.Error())
-	}
-
-	return ctx.GetStub().GetTxID(), ctx.GetStub().PutState(loan.Loan_ID, loanAsBytes)
-}
-
-// ******************************************** pay emi *************************************************************
-
-func (s *SmartContract) PayEMI(ctx contractapi.TransactionContextInterface, date string, loan_id string) (string, error) {
-	if len(date) == 0 || len(loan_id) == 0 {
-		return "", fmt.Errorf("Please pass the correct date")
-	}
-
-	loanAsBytes, err := ctx.GetStub().GetState(loan_id)
-
-	if err != nil {
-		return "", fmt.Errorf("Failed to get loan data. %s", err.Error())
-	}
-
-	if loanAsBytes == nil {
-		return "", fmt.Errorf("%s does not exist", loan_id)
-	}
-
-	loan := new(Loan)
-	_ = json.Unmarshal(loanAsBytes, loan)
-
-	for i := 0; i < len(loan.Emi); i++ {
-		if loan.Emi[i].Date == date {
-			loan.Emi[i].Payment = true
-			break
-		}
-	}
-
-	loanAsBytes, err = json.Marshal(loan)
-	if err != nil {
-		return "", fmt.Errorf("Failed while marshling loan. %s", err.Error())
-	}
-
-	return ctx.GetStub().GetTxID(), ctx.GetStub().PutState(loan.Loan_ID, loanAsBytes)
-}
-
-// ******************************************** pay tax *************************************************************
-
-func (s *SmartContract) PayTax(ctx contractapi.TransactionContextInterface, date string, loan_id string) (string, error) {
-	if len(date) == 0 || len(loan_id) == 0 {
-		return "", fmt.Errorf("Please pass the correct date")
-	}
-
-	loanAsBytes, err := ctx.GetStub().GetState(loan_id)
-
-	if err != nil {
-		return "", fmt.Errorf("Failed to get loan data. %s", err.Error())
-	}
-
-	if loanAsBytes == nil {
-		return "", fmt.Errorf("%s does not exist", loan_id)
-	}
-
-	loan := new(Loan)
-	_ = json.Unmarshal(loanAsBytes, loan)
-
-	for i := 0; i < len(loan.Tax); i++ {
-		if loan.Tax[i].Date == date {
-			loan.Tax[i].Payment = true
-			break
-		}
-	}
-
-	loanAsBytes, err = json.Marshal(loan)
-	if err != nil {
-		return "", fmt.Errorf("Failed while marshling loan. %s", err.Error())
-	}
-
-	return ctx.GetStub().GetTxID(), ctx.GetStub().PutState(loan.Loan_ID, loanAsBytes)
+	return user, nil
 }
 
 // ******************************************** main function *************************************************************
