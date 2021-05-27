f948775341a8d2d995b900726e7848c2de0912b8
jitho <jithovjoy@gmail.com>
2021-05-28 02:49:23 +0530

    sample view loans page for lenders  and getLoanForLenders function working fine

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -146,10 +146,10 @@ exports.getLoans = async (req, res, next)=>{
 
         let arr = new Array();
         let role  = (req.session.user.role == "borrower")? "borrower": "lender";
-        const query = Loan.find(); // `query` is an instance of `Query`
+        const query = Loan.find({"borrower": req.session.user.adhar_id}); // `query` is an instance of `Query`
         query.setOptions({ lean : true });
         query.collection(Loan.collection);
-        query.where(role).gte(req.session.user.adhar_id).exec(async (err, loan)=>{
+        query.exec(async (err, loan)=>{
             if(err){
                 return res.render(`/users/${role}/myloans`);
 
@@ -176,6 +176,54 @@ exports.getLoans = async (req, res, next)=>{
         })
     }
 }
+
+exports.getLoansForLender = async (req, res, next)=>{
+    if(req.session.user){
+        let chaincodeName = "loan";
+        let channelName = "mychannel";
+        let fcn = "GetLoanById";
+        let args = new Array();
+        let userName = req.session.user.adhar_id;
+        let userOrg = (req.session.user.role == "borrower")? "Org1":"Org2";
+        let trasient = "";
+
+        let arr = new Array();
+        let role  = (req.session.user.role == "borrower")? "borrower": "lender";
+        const query = Loan.find(); // `query` is an instance of `Query`
+        query.setOptions({ lean : true });
+        query.collection(Loan.collection);
+        query.exec(async (err, loan)=>{
+            if(err){
+                return res.render(`/users/${role}/myloans`);
+
+                }else if(loan){
+                    console.log("loans",loan);
+                    for(let i = 0; i < loan.length; i++){
+                        args = String(loan[i]._id);
+                        let response = await invoke.invokeTransaction(channelName, chaincodeName, fcn, args, userName, userOrg, trasient);
+                        let temp  = JSON.parse(response.result.txid)
+                        if(temp.lender === ''){
+                            arr.push(temp);
+
+                        }
+                        
+                    }
+                    res.loan = arr;
+                    console.log("response ##############", res.loan);
+                
+                }
+                next();
+               
+        });
+      
+        
+    }else{
+        return res.status(400).json({
+            error:"not logged in"
+        })
+    }
+}
+
 
 exports.invokeTransaction = (req, res) => {
     console.log("*********************invoke function************************")
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const {register, invokeTransaction, issueLoan, invokeLoan, getLoans} = require('../controllers/users');
+const {register, invokeTransaction, issueLoan, invokeLoan, getLoans, getLoansForLender} = require('../controllers/users');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -15,6 +15,10 @@ router.post('/loan', issueLoan, invokeLoan);
 
 router.get('/borrower/myloans', getLoans, function(req, res) {
   res.render('users/viewLoanBorrower', {myLoan: res.loan});
+});
+
+router.get('/lender/loans', getLoansForLender, function(req, res) {
+  res.render('users/viewLoanLender', {myLoan: res.loan});
 });
 
 router.post('/register', register);
