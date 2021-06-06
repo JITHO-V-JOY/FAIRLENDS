38393325035e6350c1d0321680238655a88e71de
jitho <jithovjoy@gmail.com>
2021-06-06 12:33:54 +0530

    view loan requests admin page

---
+++ api/controllers/admin.js
@@ -0,0 +1,52 @@
+const helper = require('../utils/helper');
+const invoke = require('../utils/invoke');
+const Loan = require('../models/loans');
+const { response } = require('express');
+
+exports.getLoansForAdmin = async (req, res, next)=>{
+    if(req.session.user){
+        let chaincodeName = "loan";
+        let channelName = "mychannel";
+        let fcn = "GetLoanById";
+        let args = new Array();
+        let userName = req.session.user.adhar_id;
+        let userOrg = (req.session.user.role == "borrower")? "Org1": (req.session.user.role == "admin")? "Org3": "Org2";
+        let trasient = "";
+
+        let arr = new Array();
+        let role  = (req.session.user.role == "borrower")? "borrower": "lender";
+        const query = Loan.find(); // `query` is an instance of `Query`
+        query.setOptions({ lean : true });
+        query.collection(Loan.collection);
+        query.exec(async (err, loan)=>{
+            if(err){
+                return res.render(`/users/${role}/myloans`);
+
+                }else if(loan){
+                    console.log("loans",loan);
+                    for(let i = 0; i < loan.length; i++){
+                        args = String(loan[i]._id);
+                        let response = await invoke.invokeTransaction(channelName, chaincodeName, fcn, args, userName, userOrg, trasient);
+                        let temp  = JSON.parse(response.result.txid)
+                        
+                        if(temp.lender === ''){
+                            arr.push(temp);
+
+                        }
+                        
+                    }
+                    res.loan = arr;
+                    console.log("response ##############", res.loan);
+                
+                }
+                next();
+               
+        });
+      
+        
+    }else{
+        return res.status(400).json({
+            error:"not logged in"
+        })
+    }
+}
--- api/routes/admins.js
+++ api/routes/admins.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const {getLoansForAdmin} = require('../controllers/admin');
 
 /* GET users listing. */
 router.get('/home', function(req, res) {
     res.render('admin/welcome');
 });
 
+router.get('/loan_requests',getLoansForAdmin, function(req, res) {
+    res.render('admin/loanRequest', {loanRequests: res.loan});
 
+});
 
 module.exports = router;
